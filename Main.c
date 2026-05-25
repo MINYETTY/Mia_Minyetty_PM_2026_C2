f867f562ef1c546b80777f90654cc58416a9529b
MINYETTY <miangelilora26@gmail.com>
2026-05-24 20:16:36 -0400

    MIA MINYETTY

--- Main.c
+++ Main.c
@@ -1,51 +1,8 @@
+Hola mundo 
+mi nombre es MIA y estoy aprendiendo a programar en C.
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
- 
-#define ARRAY_SIZE 2000
- 
- 
-int i;
-int change;
-int n;
-int a[ARRAY_SIZE];
-int buf;
-time_t seed;
- 
- 
-int main(int argc, char* argv[])
-{
-    seed = 0;
-    srand(time(&seed));
-    n = sizeof(a) / sizeof(*a);
-    for (i = 0; i <= n-1; i++)
-    {
-        a[i] = rand() % 1000;
-    }
- 
-    for (i = 0; i <= n-1; i++)
-    {
-        printf("Element desordenados a[%d]: %d\n", i, a[i]);
-    }
- 
-    change = 1;
-    while (change == 1)
-    {
-        change = 0;
-        for (i = 0; i <= n-2; i++)
-        {
-            if (a[i] > a[i+1])
-            {
-                buf = a[i];
-                a[i] = a[i+1];
-                a[i+1] = buf;
-                change = 1;
-            }
-        }
-    }
-    for (i = 0; i <= n-1; i++)
-    {
-        printf("Element a[%d]: %d\n", i, a[i]);
-    }
+int main() {
+    printf("Hola mundo \n");
+    printf("mi nombre es Mia y estoy aprendiendo a programar en C.\n");
     return 0;
 }
